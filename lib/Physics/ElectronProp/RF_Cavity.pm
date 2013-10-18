876f98d58659b585f0295cc5f41e9206ad180d4f
Ben Rickman <rickman.ben@gmail.com>
2013-10-18 15:20:57 -0500

    starting to plugin field equations for RF cavity

--- lib/Physics/ElectronProp/RF_Cavity.pm
+++ lib/Physics/ElectronProp/RF_Cavity.pm
@@ -15,8 +15,8 @@ sub _init {
 
 sub E_0		{ $_[0]->{E_0		} }
 sub mode	{ $_[0]->{mode		} }
-sub epsilon	{ $_[0]->{mu		} }
-sub mu		{ $_[0]->{epsilon	} }
+sub epsilon	{ $_[0]->{epsilon	} }
+sub mu		{ $_[0]->{mu		} }
 sub radius	{ $_[0]->{radius	} }
 
 ## E and B Fields ##
@@ -24,6 +24,7 @@ sub radius	{ $_[0]->{radius	} }
 sub B_Field {
   my $self = shift;
   my $pos  = shift;
+  my $mode = $self->mode
   my ($r,$theta,$z) = list $pos;
 }
 
@@ -33,4 +34,31 @@ sub E_Field {
   my ($r,$theta,$z) = list $pos;
 }
 
+## Field Functions ##
+
+sub TM_Fields {
+  my $self = shift;
+  my $pos  = shift;
+  my $mode_nums = shift; 
+  my ($R, $d) = map { $self->$_ } qw/radius lens_length/
+  my $gamma = 
+  my ($r,$theta,$z) = list $pos;
+  my ($m, $n, $p) = list $mode_nums;
+}
+
+my %fields = {
+  TM => \&TM_Fields,
+  TE => \&TE_Fields,
+}
+
+sub gamma_2 {
+  my $self = shift;
+  return 
+}
+
+sub omega {
+  my $self = shift;
+  my ($m, $n, $p) = $self->mode;
+  return ( sqrt( ( ()**2 + ()**2 ) / () ) ) 
+}
 1;
