fe12279552c0a66b533c14225b6fb758f2f56e4f
Ben Rickman <rickman.ben@gmail.com>
2014-06-20 16:42:21 -0500

    retreival of patch mesh is success, need to add a few things before final inclusion to simulation

--- lib/Physics/ElectronProp/Mesh_Lens.pm
+++ lib/Physics/ElectronProp/Mesh_Lens.pm
@@ -13,11 +13,12 @@ sub _init {
     tie my @data, 'Tie::File', $self->mesh_file($_) or die "Can't open " . $self->mesh_file($_) . ": $!";
     $self->{mesh_array}{$_} = \@data;
   }
-  my (undef,$zi,$zf,$zs,$yi,$yf,$ys) = split '_', $self->mesh_file($field_files[0]);
-  $ys =~ s/\.\w+$//;
+  my (undef,$zi,$zf,$zs,$ri,$rf,$rs) = split '_', $self->mesh_file($field_files[0]);
+  $rs =~ s/\.\w+$//;
   $self->{mesh_length} = $zf - $zi;
-  $self->{mesh_radius} = $yf - $yi;
-  $self->{mesh_step  } = {'z' => $zs, 'y' => $ys};
+  $self->{mesh_radius} = $rf - $ri;
+  $self->{mesh_step  } = {'z' => $zs, 'r' => $rs};
+  $self->{mesh_start } = {'z' => $zi, 'r' => $ri};
   $self->{phase} = {'B',0,'E',0} unless defined $self->{phase};
   $self->{omega} = 0 unless defined $self->{omega};
 }
@@ -27,6 +28,7 @@ sub _init {
 sub lens_type	{ 'mesh' 	}
 sub mesh_file   { $_[0]->{mesh_file	}{$_[1]} }
 sub mesh_step 	{ $_[0]->{mesh_step 	}{$_[1]} }
+sub mesh_start 	{ $_[0]->{mesh_start 	}{$_[1]} }
 sub mesh_radius	{ $_[0]->{mesh_radius	} }
 sub mesh_length	{ $_[0]->{mesh_length	} }
 sub mesh_lineup { $_[0]->{mesh_lineup	} }
@@ -41,9 +43,9 @@ sub B_Field {
   my $pos  = shift;
   my ($r,$theta,$z,$t) = list $pos;
   my $omega = $self->omega;
-  my $phase = $self->phase(B);
+  my $phase = $self->phase('B');
   if ($r <= $self->mesh_radius && $z <= $self->mesh_length + $self->front_pos && $z >= $self->front_pos) {
-    return $self->get_fields('B',($r,$z-$self->front_pos)) * cos($omega*$t - $phi);
+    return $self->get_fields('B',($r,$z-$self->front_pos)) * cos($omega*$t - $phase);
   }
   return zeros(3)
 }
@@ -53,63 +55,79 @@ sub E_Field {
   my $pos  = shift;
   my ($r,$theta,$z,$t) = list $pos;
   my $omega = $self->omega;
-  my $phase = $self->phase(E);
+  my $phase = $self->phase('E');
   if ($r <= $self->radius && $z <= $self->lens_length + $self->front_pos && $z >= $self->front_pos) {
-    return $self->get_fields('E',($r,$z-$self->front_pos)) * cos($omega*$t - $phi);
+    return $self->get_fields('E',($r,$z-$self->front_pos)) * cos($omega*$t - $phase);
   }
   return zeros(3)
 }
 
 # Field Retrieval from mesh
 
-sub get_efields {
+sub get_fields {
   my $self = shift;
   my ($field,$r,$z) = @_;
-  
-  my @field_2d;
-  for my $dir (0..1) {
-    my ($ez, $er) = ( arr_grab($pos->index(0),\@z), arr_grab($pos->index(1),\@r) );
-    my $line_test = $z + ($r * ($$ey[0]-$$ey[1]) + ($$ez[1]*$$ey[1]-$$ez[0]*$$ey[0])) / ($$ez[0] - $$ez[1]);
-    my @points;
-    for my $i (0..1) {
-      $points[$i] = pdl ($$ez[!$i], $$ey[$i],$efield{$$ez[!$i]}{$$ey[$i]}[$dir]);
-    }
-    $points[2] = pdl ($$ez[0], $$ey[0],$efield{$$ez[0]}{$$ey[0]}[$dir]);
-    if (sprintf "%.14f", $line_test > 0) {
-      $points[2] = pdl ($$ez[1], $$ey[1],$efield{$$ez[1]}{$$ey[1]}[$dir]);
-    }
-    my $plane = crossp($points[0]-$points[2],$points[1]-$points[2]);
-    $plane /= $plane->index(2);
-    push @Ef, inner($plane,$points[2]) - inner($plane,pdl ($z,$r,0));
+  my @field;
+  for ('z','r') {
+    my $pos = pdl (1,$z,$r,$z*$r);
+    my $box = $self->box_zr($field . $_,$z,$r);
+    push @field, inner($pos,$self->fit_4pt($box));
   }
-  return pdl @Ef
+  return pdl ($field[1], 0, $field[0]);
 }
 
-sub arr_grab {
+sub box_zr {
   my $self = shift;
-  my ($pos, $array) = @_;
-  my $step = $$array[1] - $$array[0];
-  my @return = grep $_ >= $pos - $step, grep $_ <= $pos + $step, @$array;
-  push @return, $return[0] + $step if @return == 1;
-  shift @return if @return == 3;
-  die "I grabbed " . @return . " elemtents when I only meant to grab 2: $!" if @return != 2;
-  return \@return;
+  my ($fn_key,$z,$r) = @_;
+  my ($i,$j) = $self->zr_ij(($z,$r));
+  my @box;
+  for my $ii ($i,$i+1) {
+    for my $jj ($j,$j+1) {
+      push @box, [$self->ij_zr($ii,$jj),$self->ij_element($fn_key,$ii,$jj)];
+    }
+  }
+  return \@box;
 }
 
-sub zy_pot {
-  return ij_pot(zy_ij(@_));
+sub fit_4pt {
+  my ($self,$pts) = @_;
+  my ($x0,$y0,$z00) = @{$$pts[0]};
+  my ($x1,$y1,$z11) = @{$$pts[3]};
+  my ($z01,$z10) = ($$pts[1][2],$$pts[2][2]);
+  my $c0 = ($x1*$y1*$z00 - $x1*$y0*$z01 - $x0*$y1*$z10 + $x0*$y0*$z11)/(($x0 - $x1)*($y0 - $y1)); 
+  my $c1 = ($y1*(-$z00 + $z10) + $y0*($z01 - $z11))/(($x0 - $x1)*($y0 - $y1)); 
+  my $c2 = ($x1*(-$z00 + $z01) + $x0*($z10 - $z11))/(($x0 - $x1)*($y0 - $y1));
+  my $c3 = ($z00 - $z01 - $z10 + $z11)/(($x0 - $x1)*($y0 - $y1));
+  pdl ($c0,$c1,$c2,$c3);
 }
 
-sub ij_pot {
-  my ($i,$j) = @_;
-  my @row = split ', ', $datafile[$i];
+sub ij_element {
+  my $self = shift;
+  my ($fn_key,$i,$j) = @_;
+  my @row = split ', ', $self->mesh_array($fn_key,$i);
   return $row[$j];
 }
-sub zy_ij {
-  my ($z, $y) = @_;
-  return (int ($z-$zi)/$zs, int ($y-$yi)/$ys);
+
+sub zr_ij {
+  my $self = shift;
+  my ($z, $r) = @_;
+  return (
+    int ($z - $self->mesh_start('z'))/$self->mesh_step('z'), 
+    int ($r - $self->mesh_start('r'))/$self->mesh_step('r')
+  );
 }
 
+sub ij_zr {
+  my $self = shift;
+  my ($i, $j) = @_;
+  return (
+    $self->mesh_start('z') + $i*$self->mesh_step('z'), 
+    $self->mesh_start('r') + $j*$self->mesh_step('r')
+  );
+}
 
 
 1;
+__END__
+
+
